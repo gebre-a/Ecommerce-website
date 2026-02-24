83a4fb20fe728104f71d034275928fceb66e95ee
gebre-a <kidanalemayehu12@gmail.com>
2026-02-24 17:09:27 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Home from './page/Home'  
+import Auth from './page/Auth'
+import Checkout from './page/Checkout'
+import Navbar from './components/Navbar'
+
+import './App.css'
+import { Routes, Route } from 'react-router-dom'
+function App() {
+ 
+
+  return (
+    <div className="app">
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/auth' element={<Auth />} />
+        <Route path='/checkout' element={<Checkout />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
